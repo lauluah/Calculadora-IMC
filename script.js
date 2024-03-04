d4785f5f55c2d39b81a69250880708b3079b91d1
Laura Marques <159571325+lauluah@users.noreply.github.com>
2024-03-04 12:26:10 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function descobrir() {
+let peso = parseInt(document.getElementById("teste1").value);
+let altura = parseInt(document.getElementById("teste2").value);
+
+let alturaEmMestros = altura / 100;
+
+let contaDoImc = peso / (alturaEmMestros * alturaEmMestros);
+
+let msg;
+
+    if (contaDoImc < 18.5) {
+          msg = "Abaixo do peso." +  `  IMC: ${contaDoImc}`;
+    } else if (contaDoImc <= 24.90) {
+          msg = "Peso normal." + ` IMC: ${contaDoImc}`;
+    } else if (contaDoImc <= 29.90) {
+          msg = "Sobrepeso." +   `  IMC: ${contaDoImc}`;
+    } else if (contaDoImc <= 34.90) {
+          msg= "Obesidade Grau I." +  `  IMC: ${contaDoImc}`;
+    } else if (contaDoImc <= 39.90) {
+          msg = "Obseidade Grau II."  +  ` IMC: ${contaDoImc}`;
+    } else {
+          msg = "Obseidade Grau III ou Mórbida."  +  ` IMC: ${contaDoImc}`;
+    }
+ document.getElementById("resultado").innerText = msg;
+};
